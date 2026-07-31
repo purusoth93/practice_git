dff4529267c857e4035e521a91b85d776aeb2665
purusoth93 <purusothaman.d17122003@gmail.com>
2026-07-31 21:41:50 +0530

    "last commit and push"

--- practice/src/main/java/com/example/practice/PracticeApplication.java
+++ practice/src/main/java/com/example/practice/PracticeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PracticeApplication {
-
+	//main method
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
 	}
