e246dde243c0c61176ba54669be46a7c1d92a762
purusoth93 <purusothaman.d17122003@gmail.com>
2026-08-18 08:49:59 +0530

    "commit 18"

--- practice/src/main/java/com/example/practice/PracticeApplication.java
+++ practice/src/main/java/com/example/practice/PracticeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PracticeApplication {
-	//main method
+	//main method starts here
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeApplication.class, args);
 	}
